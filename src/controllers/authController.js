8e5573ad50ae9f9359ddd452e14c2c88a2cbc0f0
Ryan Silva <101747305+Ryansv1@users.noreply.github.com>
2022-09-08 12:29:32 -0300

    auth

---
+++ src/controllers/authController.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/register', async (req,res) => {
+    const { email } = req.body;
+    
+    try {
+        if(await User.findOne({ email }))
+        return res.status(400).send({ error:'User Already Exists'})
+        
+        const user = await User.create(req.body);
+
+        User.password = undefined;
+
+        return res.send({ user });
+    } catch (err){
+        return res.status(400).send({ error: 'Registration Failed '});
+    }
+});
+
+module.exports = app => app.use ('/auth', router);
